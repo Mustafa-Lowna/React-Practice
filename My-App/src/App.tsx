c060dc453b222bd0a3aecd46e6704e66d8a6c33d
Mustafa Lowna <mustafalowna321@gmail.com>
2026-05-10 21:31:02 +0530

    Products List

---
+++ My-App/src/App.tsx
@@ -0,0 +1,11 @@
+import Products from "./Products";
+function App() {
+  return (
+    <div>
+      <h1>My Products App</h1>
+      <Products></Products>
+    </div>
+  );
+}
+
+export default App;
---
+++ My-App/src/Products.tsx
@@ -0,0 +1,15 @@
+function Products() {
+  return (
+    <div>
+      <h2>Products List</h2>
+      <ul>
+        <li>iPhone 15</li>
+        <li>Samsung Galaxy S21</li>
+        <li>Google Pixel 6</li>
+        <li>OnePlus 9</li>
+        <li>Xiaomi Mi 11</li>
+      </ul>
+    </div>
+  );
+}
+export default Products;
